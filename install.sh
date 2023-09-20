c36857250ca6a23ae9e92f1958274cc6153d9f5b
themamiza <drogoniza@gmail.com>
2023-09-20 22:14:40 +0330

    Changed ownership settings for home directory of new user

--- install.sh
+++ install.sh
@@ -58,10 +58,10 @@ preinstallmsg() {
 adduserandpass() {
 	whiptail --infobox "Adding user \"$name\"..." 7 50
 	useradd -m -g wheel -s /bin/zsh "$name" > /dev/null 2>&1 ||
-		usermod -a -G wheel "$name" && mkdir -p /home/"$name" && chown "$name":"$name" /home/"$name"
+		usermod -a -G wheel "$name" && mkdir -p /home/"$name" && chown "$name":wheel /home/"$name"
 	export repodir="/home/$name/.local/src"
 	mkdir -p "$repodir"
-	chown -R "$name":"$name" "$(dirname "$repodir")"
+	chown -R "$name":wheel "$(dirname "$repodir")"
 	echo "$name:$pass1" | chpasswd
 	unset pass1 pass2
 }
