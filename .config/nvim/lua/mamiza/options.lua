7541970d9766a1da9065cfc5b3caacae7d6ef0c3
themamiza <drogoniza@gmail.com>
2024-08-27 11:54:37 +0330

    Added autocmd to restore cursor position. (see `:help last-position-jump`)

--- .config/nvim/lua/mamiza/options.lua
+++ .config/nvim/lua/mamiza/options.lua
@@ -49,3 +49,15 @@ set.timeoutlen = 300
 
 -- How to complete:
 set.completeopt = "menuone,noselect"
+
+vim.cmd [[
+        augroup RestoreCursor
+        autocmd!
+        autocmd BufRead * autocmd FileType <buffer> ++once
+        \ let s:line = line("'\"")
+        \ | if s:line >= 1 && s:line <= line("$") && &filetype !~# 'commit'
+        \      && index(['xxd', 'gitrebase'], &filetype) == -1
+        \ |   execute "normal! g`\""
+        \ | endif
+        augroup END
+]]
