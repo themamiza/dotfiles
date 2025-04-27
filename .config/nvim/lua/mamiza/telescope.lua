da093d24a16a72092fa4922e039e735fd40d1dd8
themamiza <drogoniza@gmail.com>
2025-04-28 00:32:11 +0330

    Rewrite telescope configuration to include dotfiles

--- .config/nvim/lua/mamiza/telescope.lua
+++ .config/nvim/lua/mamiza/telescope.lua
@@ -1,12 +1,27 @@
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>.", builtin.find_files, {})
-require("telescope").setup {
-        defaults = {
-                mappings = {
-                        i = {
-                                ["<C-u>"] = false,
-                                ["<C-d>"] = false,
-                        }
+local telescope = require("telescope")
+local telescopeConfig = require("telescope.config")
+
+local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+table.insert(vimgrep_arguments, "--hidden")
+table.insert(vimgrep_arguments, "--glob")
+table.insert(vimgrep_arguments, "!**/.git/*")
+
+telescope.setup({
+	defaults = {
+		-- `hidden = true` is not supported in text grep commands.
+		vimgrep_arguments = vimgrep_arguments,
+        mappings = {
+                i = {
+                        ["<C-u>"] = false,
+                        ["<C-d>"] = false,
                 }
         }
-}
+	},
+	pickers = {
+		find_files = {
+			-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+			find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+		},
+	},
+})
