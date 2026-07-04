17fdd533f5bd24baea6a67276ec7e7cdc10f9d2f
themamiza <drogoniza@gmail.com>
2026-07-04 17:45:26 +0330

    Add completion capabilities to all language servers

--- .config/nvim/lua/mamiza/lsp.lua
+++ .config/nvim/lua/mamiza/lsp.lua
@@ -26,11 +26,23 @@ vim.lsp.config("lua_ls", {
 vim.lsp.enable("lua_ls")
 
 -- simple lsp setups
+vim.lsp.config("bashls", {
+  capabilities = require("cmp_nvim_lsp").default_capabilities(),
+})
 vim.lsp.enable("bashls")
+
+vim.lsp.config("clangd", {
+  capabilities = require("cmp_nvim_lsp").default_capabilities(),
+})
 vim.lsp.enable("clangd")
+
+vim.lsp.config("pylsp", {
+  capabilities = require("cmp_nvim_lsp").default_capabilities(),
+})
 vim.lsp.enable("pylsp")
 
 vim.lsp.config("qmlls", {
+  capabilities = require("cmp_nvim_lsp").default_capabilities(),
   cmd = { "qmlls6" } -- Correct binary name
 })
 vim.lsp.enable("qmlls")
